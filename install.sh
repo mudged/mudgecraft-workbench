2f541d3af1bf42d5abfc96a6041f5c278b132346
Dan Mudge <dmudge@gmail.com>
2023-03-23 12:20:13 +0000

    fix line endings

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ DOCKER_GID=$(getent group docker | awk -F: '{print $3}')
 docker run --rm --detach --pull always \
     --name mudgecraft-workbench \
     -p 80:3400 \
-    -v mudgecraft-workspace-data:/data/workspace
-    -v mudgecraft-minecraft-server-data:/data/minecraft-server
+    -v mudgecraft-workspace-data:/data/workspace \
+    -v mudgecraft-minecraft-server-data:/data/minecraft-server \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --group-add ${DOCKER_GID} \
     ghcr.io/mudged/mudgecraft-workbench:main
